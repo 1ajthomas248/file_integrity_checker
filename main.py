35fd092a9f3920ea78d088229e02a58a0da19ebd
1ajthomas248 <1ajthomas248@gmail.com>
2025-12-13 00:45:26 -0500

    define cmd_verify function

--- main.py
+++ main.py
@@ -18,3 +18,26 @@ def cmd_hash(args: argparse.Namespace) -> int:
     print(f"SHA-256: {digest}")
     return 0
 
+def cmd_verify(args: argparse.Namespace) -> int:
+    file_path = args.file
+    expected_hash = args.expected_hash
+
+    try:
+        is_match, actual_hash = verify_hash(file_path, expected_hash)
+    except FileNotFoundError as e:
+        print(f"No file {e}", file=sys.stderr)
+        return 1
+    
+    print(f"File:     {file_path}")
+    print(f"Expected: {expected_hash}")
+    print(f"Actual:   {actual_hash}")
+    print()
+
+    if is_match: 
+        print("Integrity check PASSED (hashes match)")
+        return 0
+    else:
+        print("Integrity check FAILED (hahed do NOT match)")
+        return 2
+    
+    
